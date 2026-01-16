c57887893405fdb2a69bb786bf94c8f7bdaf14c0
Камилла Губайдуллина <gubai.k60@gmail.com>
2026-01-16 14:18:54 +0500

    Создание uikit

--- lib/src/styles.dart
+++ lib/src/styles.dart
@@ -4,97 +4,97 @@ import 'package:package_uikit/src/color.dart';
 
 // Губайдуллина Камилла, 16.01.2026 12:34, основные текстовые стили для проекта
 
-var title1semi = GoogleFonts.roboto(
+final title1semi = GoogleFonts.roboto(
   fontSize: 24,
   fontWeight: FontWeight.w700,
   color: black,
 );
 
-var title1extraBold = GoogleFonts.roboto(
+final title1extraBold = GoogleFonts.roboto(
   fontSize: 24,
   fontWeight: FontWeight.bold,
   color: black,
 );
 
-var title2regular = GoogleFonts.roboto(
+final title2regular = GoogleFonts.roboto(
   fontSize: 20,
   fontWeight: FontWeight.w500,
   color: black,
 );
 
-var title2semi = GoogleFonts.roboto(
+final title2semi = GoogleFonts.roboto(
   fontSize: 20,
   fontWeight: FontWeight.w700,
   color: black,
 );
 
-var title2extraBold = GoogleFonts.roboto(
+final title2extraBold = GoogleFonts.roboto(
   fontSize: 20,
   fontWeight: FontWeight.bold,
   color: black,
 );
 
-var title3regular = GoogleFonts.roboto(
+final title3regular = GoogleFonts.roboto(
   fontSize: 17,
   fontWeight: FontWeight.w500,
   color: black,
 );
 
-var title3medium = GoogleFonts.roboto(
+final title3medium = GoogleFonts.roboto(
   fontSize: 17,
   fontWeight: FontWeight.w600,
   color: black,
 );
 
-var title3semi = GoogleFonts.roboto(
+final title3semi = GoogleFonts.roboto(
   fontSize: 17,
   fontWeight: FontWeight.w700,
   color: black,
 );
 
-var headlineRegular = GoogleFonts.roboto(
+final headlineRegular = GoogleFonts.roboto(
   fontSize: 16,
   fontWeight: FontWeight.w500,
   color: black,
 );
 
-var headlineMedium = GoogleFonts.roboto(
+final headlineMedium = GoogleFonts.roboto(
   fontSize: 16,
   fontWeight: FontWeight.w600,
   color: black,
 );
 
-var textRegular = GoogleFonts.roboto(
+final textRegular = GoogleFonts.roboto(
   fontSize: 15,
   fontWeight: FontWeight.w500,
   color: black,
 );
 
-var textMedium = GoogleFonts.roboto(
+final textMedium = GoogleFonts.roboto(
   fontSize: 15,
   fontWeight: FontWeight.w600,
   color: black,
 );
 
-var captionRegular = GoogleFonts.roboto(
+final captionRegular = GoogleFonts.roboto(
   fontSize: 14,
   fontWeight: FontWeight.w500,
   color: black,
 );
 
-var captionSemi = GoogleFonts.roboto(
+final captionSemi = GoogleFonts.roboto(
   fontSize: 14,
   fontWeight: FontWeight.w700,
   color: black,
 );
 
-var caption2Regular = GoogleFonts.roboto(
+final caption2Regular = GoogleFonts.roboto(
   fontSize: 12,
   fontWeight: FontWeight.w700,
   color: black,
 );
 
-var caption2Bold = GoogleFonts.roboto(
+final caption2Bold = GoogleFonts.roboto(
   fontSize: 12,
   fontWeight: FontWeight.w800,
   color: black,
