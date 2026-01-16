196d1e3648d6b7875a14da0dc2c988f3c05606c6
Камилла Губайдуллина <gubai.k60@gmail.com>
2026-01-16 17:22:17 +0500

    Создание uikit

--- lib/src/widget/custom_tabbar.dart
+++ lib/src/widget/custom_tabbar.dart
@@ -5,9 +5,9 @@ import 'package:package_uikit/src/styles.dart';
 // Губайдуллина Камилла, 16.01.2026 13:10 , навигация для ввода данных в проекте
 class CustomTabBar extends StatefulWidget {
   final List<String> icon; // Список иконок
-  final List<String> title;  // Список текстов
-  final ValueChanged<int> onPressed;  // Действие при нажатии
-  final int selectedIndex;  // Выбранное значение
+  final List<String> title; // Список текстов
+  final ValueChanged<int> onPressed; // Действие при нажатии
+  final int selectedIndex; // Выбранное значение
 
   const CustomTabBar({
     super.key,
@@ -25,6 +25,9 @@ class _CustomTabBarState extends State<CustomTabBar> {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      onTap: (index) {
+        widget.onPressed(index);
+      },
       type: BottomNavigationBarType.fixed,
       selectedItemColor: accent,
       selectedLabelStyle: caption2Regular.copyWith(color: accent),
@@ -34,7 +37,12 @@ class _CustomTabBarState extends State<CustomTabBar> {
       items: List.generate(
         widget.title.length,
         (index) => BottomNavigationBarItem(
-          icon: Image.asset(widget.icon[index], width: 32, height: 32,),
+          icon: Image.asset(
+            widget.icon[index],
+            width: 32,
+            height: 32,
+            color: widget.selectedIndex == index ? accent : icons,
+          ),
           label: widget.title[index],
         ),
       ),
