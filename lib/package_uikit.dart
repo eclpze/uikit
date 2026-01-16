755afc30d836d90e57a5fb3e3c824ffd92fbe627
Камилла Губайдуллина <gubai.k60@gmail.com>
2026-01-16 14:05:31 +0500

    Создание uikit

--- lib/package_uikit.dart
+++ lib/package_uikit.dart
@@ -1,3 +1,11 @@
-library uikit;
+library package_uikit;
 
-export '';
+export 'src/color.dart';
+export 'src/styles.dart';
+export 'src/widget/custom_button.dart';
+export 'src/widget/custom_dropdown.dart';
+export 'src/widget/custom_textfield.dart';
+export 'src/widget/custom_icon.dart';
+export 'src/widget/custom_search.dart';
+export 'src/widget/custom_tabbar.dart';
+
